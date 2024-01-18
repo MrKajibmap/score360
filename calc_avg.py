fe63ba9d7adeae8259e5858bf34c086081ec9d56
MrKajibmap <nik.borz@mail.ru>
2024-01-18 20:10:24 +0300

    Единый файл собран, оценки - селф, коллеги, общая

--- calc_avg.py
+++ calc_avg.py
@@ -20,6 +20,9 @@ if __name__ == '__main__':
             df_current_employee_hard = pd.read_excel(file)
             df_current_employee_hard = df_current_employee_hard.loc[:,
                                        ~df_current_employee_hard.columns.str.contains('^Unnamed')]
+            # убираем владельца файла, чтобы не учитывать его в средней оценке
+            df_current_employee_hard = df_current_employee_hard.loc[:,
+                                       ~df_current_employee_hard.columns.str.contains(str('^' + current_employee))]
             count_row = df_current_employee_hard.shape[0]  # Gives number of rows
             count_col = df_current_employee_hard.shape[1]  # Gives number of columns
             list_cnt_not_null_val = []
@@ -48,7 +51,7 @@ if __name__ == '__main__':
             df_current_employee_hard['Sum'] = list_col_sum
             df_current_employee_hard['Avg'] = list_col_avg
             df_current_employee_hard.to_excel(str('Output_files/Results_with_stats/' + file.name))
-            #
+            #     #
             df_current_employee_with_stat = pd.read_excel(str('Output_files/Results_with_stats/' + file.name))
             df_current_employee_with_stat = df_current_employee_with_stat.loc[:,
                                             ~df_current_employee_with_stat.columns.str.contains('^Unnamed')]
@@ -67,19 +70,63 @@ if __name__ == '__main__':
             df_current_empl['Soft_Avg_Score'] = round(df_current_employee_with_stat.iloc[17:31, count_col - 1].sum(), 1)
             df_current_empl['Soft_Scores_Cnt'] = round(df_current_employee_with_stat.iloc[17:31, count_col - 3].mean(),
                                                        1)
-            new_df = pd.read_excel('Output_files/Results_with_stats/' + file.name)
+            new_df = pd.read_excel('Output_files/Res_Hard_Soft/' + file.name)
             new_df = new_df.loc[:,
                      new_df.columns.str.contains(str('^' + str(file.name).split(sep='.')[0]))]
             df_current_empl['Self_Hard'] = new_df.iloc[0:17, 0].sum()
             df_current_empl['Self_Soft'] = new_df.iloc[17:31, 0].sum()
             df_current_empl['Self_Total'] = new_df.iloc[0:31, 0].sum()
-            df_current_empl.to_excel(str('Output_files/Templ/' + file.name))
+            df_current_empl.to_excel(str('Output_files/Self_Team_ws_Stat/' + file.name))
             # print(df_current_empl)
             if main_iter == 1:
+                # для первой итерации  создаем файл с первым сотрудником
                 general_score_stat_df = df_current_empl
                 # print(general_score_stat_df)
                 # print('PRIVET')
             else:
+                # накапливаем статистику по каждому сотруднику (проходясь по файлам) в итоговый ДФ
                 general_score_stat_df = pd.concat([general_score_stat_df, df_current_empl])
                 # print(general_score_stat_df)
-        general_score_stat_df.to_excel(str('Output_files/Templ/general_score_stat_df.xlsx'))
+            # дообогащение файла селф оценкой
+            df_current_employee_with_stat_added = pd.read_excel(str('Output_files/Results_with_stats/' + file.name))
+            # df_current_employee_with_stat_added = pd.DataFrame(data=df_current_employee_with_stat_added.loc[:,
+            #                                                         ~df_current_employee_with_stat_added.columns.str.contains(
+            #                                                             '^Крит')])
+            df_current_employee_with_stat_added = pd.DataFrame(data=df_current_employee_with_stat_added.loc[:,
+                                                                    ~df_current_employee_with_stat_added.columns.str.contains(
+                                                                        '^Unnamed')])
+            # забираем самооценку
+            df_current_employee_self_assessment = pd.read_excel(str('Output_files/Res_Hard_Soft/' + file.name))
+            df_current_employee_self_assessment = pd.DataFrame(data=df_current_employee_self_assessment.loc[:,
+                                                                    df_current_employee_self_assessment.columns.str.contains(
+                                                                        current_employee)])
+            # добавляем ее к оценкам от коллег + статистика
+            df_current_employee_with_stat_added.insert(1, current_employee, df_current_employee_self_assessment.loc[:,
+                                                                            df_current_employee_self_assessment.columns.str.contains(
+                                                                                current_employee)])
+
+            df_current_employee_with_stat_added.to_excel(str('Output_files/Self_Team_ws_Stat/' + file.name))
+
+            # df_current_employee_self_assessment = pd.DataFrame(data=df_current_employee_self_assessment.loc[1, df_current_employee_self_assessment.columns.str.contains()
+            # добавление на отдельные странички всех сотрудников в финальный файл
+
+        # general_score_stat_df = pd.DataFrame(data=general_score_stat_df.loc[:,
+        #                                           ~general_score_stat_df.columns.str.contains(
+        #                                               str('1'))])
+        general_score_stat_df.to_excel(str('Output_files/Results/dev_scores_results.xlsx'))
+
+
+    for file in Path('Output_files/Results_with_stats').iterdir():
+        if file.name != '.DS_Store' and file.name != '':
+            current_employee = str(file.name).split(sep='.')[0]
+            df_current_employee_with_stat = pd.read_excel(file)
+            df_current_employee_with_stat = df_current_employee_with_stat.loc[:,
+                                            ~df_current_employee_with_stat.columns.str.contains('^Unnamed')]
+            print('>> << >> << ' , current_employee)
+            with pd.ExcelWriter(
+                    'Output_files/Results/dev_scores_results.xlsx',
+                    mode="a",
+                    engine="openpyxl",
+                    if_sheet_exists="new",
+            ) as writer:
+                df_current_employee_with_stat.to_excel(writer, sheet_name=current_employee)
